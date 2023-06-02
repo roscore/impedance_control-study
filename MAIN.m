28214916f05ba6c410da8627e83c1debed2517ea
roscore <roscore@kakao.com>
2023-06-02 18:09:47 +0900

    first

---
+++ MAIN.m
@@ -0,0 +1,44 @@
+clc; close all;
+
+clear all;
+
+rederive = false;
+%%%%%%%% System Parameters %%%%%%%%
+
+%Initial conditions:
+p.init = [pi/4    0.0    pi/4  0.0]';
+
+p.g = 9.81;
+p.m1 = 1; %Mass of link 1.
+p.m2 = 1; %Mass of link 2.
+p.l1 = 1; %Total length of link 1.
+p.l2 = 1; %Total length of link 2.
+p.d1 = p.l1/2; %Center of mass distance along link 1 from the fixed joint.
+p.d2 = p.l2/2; %Center of mass distance along link 2 from the fixed joint.
+p.I1 = 1/12*p.m1*p.l1^2; %Moment of inertia of link 1 about COM
+p.I2 = 1/12*p.m2*p.l2^2; %Moment of inertia of link 2 about COM
+
+endZ = ForwardKin(p.l1,p.l2,p.init(1),p.init(3));
+x0 = endZ(1); %End effector initial position in world frame.
+y0 = endZ(2);
+p.Fx = 0;
+p.Fy = 0;
+
+%%%%%%%% Control Parameters %%%%%%%%
+
+%Controller Gains
+p.Kp = 200;
+p.Kd = 10;
+
+%Single target:
+p.xtarget = x0;
+p.ytarget = y0;
+
+%%%%%%%% Run Derivers %%%%%%%%
+
+if rederive
+        deriverRelativeAngles;
+        disp('Equations of motion and control parameters derived using relative angles.');
+end
+
+Plotter(p) 
